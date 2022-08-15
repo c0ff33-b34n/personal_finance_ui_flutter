d0758e17d3f2f2fabdc6c0b7c3b6aae0a7c8eb2d
Stuart Bowes <stuartbowes2000@gmail.com>
2022-08-15 10:37:21 +0100

    Initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+# personal_finance_tracking_ui
+
+Personal finance mobile app UI re-created from 3rd party designs.
+
---
+++ android/app/src/main/java/com/example/personal_finance_tracking_ui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.personal_finance_tracking_ui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
