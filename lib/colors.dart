4b6360a1907124ce8fbd117601910284e0946c76
Stuart Bowes <stuartbowes2000@gmail.com>
2022-08-15 15:51:22 +0100

    Add colors.dart with colors for dark theme

---
+++ lib/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundPrimaryDark = Color(0xFF020236);
+const kBackgroundSecondaryDark = Color(0xFF1B1C4A);
+const kTextColorPrimaryDark = Color(0xFFFFFFFF);
+const kTextColorSecondaryDark = Color(0xFF7D7BAC);
+
+const kButtonGradientTopDark = Color(0xFF3E3E70);
+const kButtonGradientBottomDark = Color(0xFF1A1A4C);
+
+const kTextRed = Color(0xFFC62976);
+const kTextGreen = Color(0xFF51B6A6);
