fff5a4578aa8e74b6990d994a088afc0aae1d5d9
Stuart Bowes <stuartbowes2000@gmail.com>
2022-08-15 16:44:41 +0100

    Apply app color scheme to existing components

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'colors.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
@@ -22,6 +24,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
+          color: kBackgroundPrimaryDark,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
@@ -33,6 +36,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
+      theme: _buildAppTheme(),
     );
   }
 }
@@ -49,6 +53,9 @@ Widget userMessage = Container(
               padding: const EdgeInsets.only(bottom: 8),
               child: const Text(
                 'Hello Martin,',
+                style: TextStyle(
+                  color: kTextColorSecondaryDark,
+                ),
               ),
             ),
             const Text(
@@ -60,7 +67,7 @@ Widget userMessage = Container(
           ],
         ),
       ),
-      Icon(Icons.notifications),
+      Icon(Icons.notifications, color: kTextColorPrimaryDark),
     ],
   ),
 );
@@ -72,15 +79,23 @@ Widget searchBar = Container(
       contentPadding: const EdgeInsets.symmetric(vertical: 15.0),
       border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20.0),
-          borderSide: const BorderSide(width: 0.8)),
+          borderSide: const BorderSide(
+            width: 0.8,
+          )),
       enabledBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(20.0),
-        borderSide: BorderSide(width: 0.8, color: Colors.black),
+        borderSide: BorderSide(width: 2.0, color: kBackgroundSecondaryDark),
       ),
       hintText: 'Transactions, services or contracts',
+      hintStyle: TextStyle(
+        color: kTextColorSecondaryDark,
+      ),
+      fillColor: kBackgroundSecondaryDark,
+      filled: true,
       prefixIcon: Icon(
         Icons.search,
         size: 30.0,
+        color: kTextColorSecondaryDark,
       ),
     ),
   ),
@@ -98,6 +113,9 @@ Widget balance = Container(
               padding: const EdgeInsets.only(bottom: 8),
               child: const Text(
                 'Total Balance',
+                style: TextStyle(
+                  color: kTextColorSecondaryDark,
+                ),
               ),
             ),
             const Text(
@@ -137,7 +155,7 @@ Widget cardsHeader = Container(
         children: [
           Container(
             height: 42.0,
-            width: 62.0,
+            width: 72.0,
             padding: const EdgeInsets.all(0.0),
             alignment: Alignment.bottomCenter,
             decoration: BoxDecoration(
@@ -146,18 +164,18 @@ Widget cardsHeader = Container(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 colors: <Color>[
-                  Colors.black45,
-                  Colors.black54,
+                  kButtonGradientTopDark,
+                  kButtonGradientBottomDark,
                 ],
               ),
             ),
           ),
           Container(
             height: 40.0,
-            width: 60.0,
+            width: 70.0,
             padding: EdgeInsets.zero,
             decoration: BoxDecoration(
-              color: Colors.lightBlue,
+              color: kBackgroundSecondaryDark,
               borderRadius: BorderRadius.circular(13.0),
             ),
             child: IconButton(
@@ -171,3 +189,19 @@ Widget cardsHeader = Container(
     ],
   ),
 );
+
+final ThemeData _kShrineTheme = _buildAppTheme();
+
+ThemeData _buildAppTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    textTheme: _buildAppTextTheme(base.textTheme),
+  );
+}
+
+TextTheme _buildAppTextTheme(TextTheme base) {
+  return base.apply(
+    displayColor: kTextColorPrimaryDark,
+    bodyColor: kTextColorPrimaryDark,
+  );
+}
