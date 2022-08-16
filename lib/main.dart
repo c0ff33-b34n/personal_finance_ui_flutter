ef33ed1f512856458f41ba2936bf58bd142a4b27
Stuart Bowes <stuartbowes2000@gmail.com>
2022-08-16 16:19:53 +0100

    Refactored PaymentCardWidget into separate class file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'colors.dart';
-import 'models/card_model.dart';
+import 'payment_card.dart';
 import 'models/cards_repository.dart';
 
 void main() {
@@ -195,132 +195,6 @@ Widget cardsHeader = Container(
 
 PaymentCardWidget individualPaymentCard =
     PaymentCardWidget(card: PaymentCardsRepository.loadPaymentCards()[0]);
-
-class PaymentCardWidget extends StatelessWidget {
-  const PaymentCardWidget({required this.card, Key? key}) : super(key: key);
-
-  final PaymentCard card;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 214,
-      width: 340,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20.0), color: kGradientPink),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20.0),
-          gradient: const LinearGradient(
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
-            // Add one stop for each color. Stops should increase from 0 to 1
-            stops: [0.0, 1.0],
-            colors: [
-              // Colors are easy thanks to Flutter's Colors class.
-              kGradientTransparent,
-              kGradientPoint5,
-            ],
-          ),
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(18),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20.0),
-            gradient: const LinearGradient(
-              // Where the linear gradient begins and ends
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              // Add one stop for each color. Stops should increase from 0 to 1
-              stops: [0.1, 0.2, 0.3, 0.40, 0.46, 0.55, 0.6, 0.7, 0.8, 0.9, 1.0],
-              colors: [
-                // Colors are easy thanks to Flutter's Colors class.
-                kGradientPoint1,
-                kGradientPoint2,
-                kGradientPoint3,
-                kGradientPoint2,
-                kGradientPoint5,
-                kGradientPoint4,
-                kGradientPoint5,
-                kGradientPoint4,
-                kGradientPoint3,
-                kGradientPoint2,
-                kGradientTransparent,
-              ],
-            ),
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                flex: 3,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      card.cardType == CardType.Visa
-                          ? 'Debit Card'
-                          : 'Credit Card',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: Text(
-                            card.cardNumber,
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Text(
-                          'Valid Thru ' + card.expiryDate,
-                          style: TextStyle(
-                            fontSize: 16.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    card.cardType == CardType.Visa
-                        ? Padding(
-                            padding: const EdgeInsets.only(bottom: 2.0),
-                            child: Image.asset(
-                              'assets/visa_logo_sm.png',
-                              alignment: Alignment.bottomRight,
-                              width: 76.0,
-                              height: 50.0,
-                            ),
-                          )
-                        : Image.asset(
-                            'assets/mastercard_logo_sm.png',
-                            alignment: Alignment.bottomRight,
-                            width: 76.0,
-                            height: 50.0,
-                          ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 ThemeData _buildAppTheme() {
   final ThemeData base = ThemeData.light();
